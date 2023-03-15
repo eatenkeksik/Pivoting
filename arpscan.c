b7868755f33daf81d21638b1ed7518023f375c09
Maria Klimova <127546036+eatenkeksik@users.noreply.github.com>
2023-03-15 17:41:33 +0400

    Update arpscan.c

--- arpscan.c
+++ arpscan.c
@@ -47,7 +47,9 @@ int main(int argc, char **argv)
     struct sockaddr_in broadcast_addr;
     memset(&broadcast_addr, 0, sizeof(broadcast_addr));
     broadcast_addr.sin_family = AF_INET;
+    broadcast_addr.sin_port = htons(0);
     broadcast_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
     if(sendto(sockfd, &request, sizeof(request), 0, (struct sockaddr *)&broadcast_addr, sizeof(broadcast_addr)) < 0) {
         perror("sendto");
         close(sockfd);
